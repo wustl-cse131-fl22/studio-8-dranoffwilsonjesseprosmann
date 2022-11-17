0f6fb2e7219384d740254fd6fe4e9c8627821e57
charlesdranoff <charlesdranoff@localhost>
2022-11-17 12:45:47 -0600

    Studio 8

--- src/studio8/Date.java
+++ src/studio8/Date.java
@@ -1,9 +1,104 @@
 package studio8;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+
 public class Date {
+	private int month, day, year;
+	private boolean holiday;
 
 
-    public static void main(String[] args) {
+    public Date(int month, int day, int year, boolean holiday) {
+		
+		this.month = month;
+		this.day = day;
+		this.year = year;
+		this.holiday = holiday;
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(day, holiday, month, year);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Date other = (Date) obj;
+		return day == other.day && holiday == other.holiday && month == other.month && year == other.year;
+	}
+
+
+	public int getMonth() {
+		return month;
+	}
+
+
+	public int getDay() {
+		return day;
+	}
+
+
+	public int getYear() {
+		return year;
+	}
+
+
+	public void setHoliday(boolean holiday) {
+		this.holiday = holiday;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Date [month=" + month + ", day=" + day + ", year=" + year + ", holiday=" + holiday + "]";
+	}
+
+	
+	
+
+	public static void main(String[] args) {
+		
+		/*
+		LinkedList<Date> list = new LinkedList<Date>();
+		
+		Date d1 = new Date(12,25,2001,true);
+		Date d2 = new Date(10,21,1999,false);
+		Date d3 = new Date(5,6,2021,false);
+		Date d4 = new Date(10,31,2003,true);
+		Date d5 = new Date(3,31,2003,true);
+		list.add(d1);
+		list.add(d2);
+		list.add(d3);
+		list.add(d4);
+		list.add(d5);
+		System.out.println(list);
+		*/
+		
+		HashSet<Date> set = new HashSet<Date>();
+		Date d1 = new Date(12,25,2001,true);
+		Date d2 = new Date(10,21,1999,false);
+		Date d3 = new Date(5,6,2021,false);
+		Date d4 = new Date(10,31,2003,true);
+		Date d5 = new Date(3,31,2003,true);
+		set.add(d1);
+		set.add(d2);
+		set.add(d3);
+		set.add(d4);
+		set.add(d5);
+		System.out.println(set);
+		
+
+		
 
     }
 
--- src/studio8/Time.java
+++ src/studio8/Time.java
@@ -1,11 +1,87 @@
 package studio8;
 
+import java.util.HashSet;
+import java.util.Objects;
 
 public class Time {
 
+	//Create instance variables
+	private int hours, minutes;
+	private boolean military;
+
+	//Create constructor
+	public Time(int hours, int minutes, boolean military) {
+		this.hours = hours;
+		this.minutes = minutes;
+		this.military = military;
+	}
+
+	//Generate hash code and equals
+	@Override
+	public int hashCode() {
+		return Objects.hash(hours, military, minutes);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Time other = (Time) obj;
+		return hours == other.hours && military == other.military && minutes == other.minutes;
+	}
+
+
+
+	//Generate getters and setters
+	public int getHours() {
+		return hours;
+	}
+
+	public int getMinutes() {
+		return minutes;
+	}
+
+	public void setMilitary(boolean military) {
+		this.military = military;
+	}
+
+	//generate toString
+	@Override
+	public String toString() {
+		if (military ==true){
+			return "Time [hours=" + hours + ", minutes=" + minutes + "]";
+		}
+		else {
+			if (hours>12) {
+				return "Time [hours=" + hours%12 + ", minutes=" + minutes + " pm " + "]";
+			}
+			else {
+				return "Time [hours=" + hours%12 + ", minutes=" + minutes + " am " + "]";
+			}
+		}
+	}
+
 	public static void main(String[] args) {
- 
-    	
-    }
+		HashSet<Time> set1 = new HashSet<Time>();
+		Time t1 = new Time(13,25,true);
+		Time t2 = new Time(10,21,false);
+		Time t3 = new Time(5,56,false);
+		Time t4 = new Time(21,37,false);
+		Time t5 = new Time(8,45,true);
+		set1.add(t1);
+		set1.add(t2);
+		set1.add(t3);
+		set1.add(t4);
+		set1.add(t5);
+		System.out.println(set1);
+
+
+	}
+
+
 
 }
